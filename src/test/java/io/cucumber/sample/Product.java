66556ba06e6a07140308112502dfb7012279428d
Gaspar Hermán <gaspar@gaspar-vit-p3400.home>
2023-12-11 18:03:45 +0100

    - Removed unnused setters

--- src/test/java/io/cucumber/sample/Product.java
+++ src/test/java/io/cucumber/sample/Product.java
@@ -18,23 +18,12 @@ public class Product {
     return description;
   }
 
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
   public int getPrice() {
     return price;
-  }
-
-  public void setPrice(int price) {
-    this.price = price;
   }
 
   public int getQuantities() {
     return quantities;
   }
 
-  public void setQuantities(int quantities) {
-    this.quantities = quantities;
-  }
 }
